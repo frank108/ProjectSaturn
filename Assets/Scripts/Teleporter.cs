c2a4b3c0a7cfba81d82e39c7f036f7aafafdc6d5
frank108 <psychicmoose103@gmail.com>
2016-11-30 15:22:42 -0500

    I was working on the teleporter and shield.

---
+++ Assets/Scripts/Teleporter.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Teleporter : MonoBehaviour
+{
+    void OnTriggerExit(Collider other)
+    {
+        other.transform.position = new Vector3(-63, 10, 0);
+        print("Tele");
+    }
+}
---
+++ Assets/Teleporter_2.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Teleporter_2 : MonoBehaviour
+{
+    void OnTriggerExit(Collider other)
+    {
+        other.transform.position = new Vector3(21, 8, 0);
+        print("Tele2");
+    }
+}
+
